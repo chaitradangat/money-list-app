e4bb76e6d590729d0a2dbd259ed124a04ec1d529
Chaitra Dangat <chadangat@gmail.com>
2023-02-05 21:09:33 +0530

    testing ef core code for dbcontext

--- money-list-app.API/Program.cs
+++ money-list-app.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using money_list_app.Data;
+
 namespace money_list_app.API
 {
     public class Program
@@ -12,6 +15,12 @@ namespace money_list_app.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                )) ;
+
+
 
             var app = builder.Build();
 
