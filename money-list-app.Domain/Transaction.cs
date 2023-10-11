792c86bab5f34b98dd18519983bf279c0531e967
Chaitra <chadangat@gmail.com>
2023-10-12 02:39:51 +0530

    created transaction domain class

---
+++ money-list-app.Domain/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money_list_app.Domain
+{
+    public class Transaction
+    {
+       public int ID { get; set; }
+
+       public DateTime DateOfTransaction { get; set; }
+
+
+
+    }
+}
